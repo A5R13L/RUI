eb7222cd0f939b0d9812719495fc749cc4dbcea1
Asriel <asriel@asriel.dev>
2022-05-28 05:35:36 -0400

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -648,42 +648,6 @@ function Library:CreateWindow(Config, Parent)
                     end
                 end)
 
-                if type(OptionTable) == "function" then
-                    OptionTable = OptionTable()
-                end
-                
-                for _, OptionName in pairs(OptionTable) do
-                    local Option = Folder.Option:Clone()
-                    Option.Name = OptionName
-                    Option.Parent = Dropdown.Container.Holder.Container
-                    Option.Title.Text = OptionName
-                    Option.BackgroundColor3 = Config.Color
-                    Option.Size = UDim2.new(1, 0, 0, Option.Title.TextBounds.Y + 5)
-                    Dropdown.Container.Holder.Size = UDim2.new(1, -5, 0, Dropdown.Container.Holder.Container.ListLayout.AbsoluteContentSize.Y)
-                    table.insert(Library.ColorTable, Option)
-
-                    Option.MouseButton1Down:Connect(function()
-                        Option.BackgroundTransparency = 0
-                    end)
-
-                    Option.MouseButton1Up:Connect(function()
-                        Option.BackgroundTransparency = 1
-                    end)
-
-                    Option.MouseLeave:Connect(function()
-                        Option.BackgroundTransparency = 1
-                    end)
-
-                    Option.MouseButton1Click:Connect(function()
-                        Dropdown.Container.Value.Text = OptionName
-                        Callback(OptionName)
-
-                        DropdownToggle = false
-                        Dropdown.Size = UDim2.new(1, -10, 0, Dropdown.Title.TextBounds.Y + 25)
-                        Dropdown.Container.Holder.Visible = false
-                    end)
-                end
-
                 function DropdownInit:AddToolTip(Name)
                     if tostring(Name):gsub(" ", "") ~= "" then
                         Dropdown.MouseEnter:Connect(function()
@@ -707,6 +671,48 @@ function Library:CreateWindow(Config, Parent)
                     if not UseCallback then return end
 
                     Callback(Name)
+                end
+
+                function DropdownInit:SetOptions(Options)
+                    if not Options then Options = OptionsTable end
+
+                    if type(Options) == "function" then
+                        Options = Options()
+                    end
+
+                    Dropdown.Container.Holder.Container:ClearAllChildren()
+
+                    for _, OptionName in pairs(Options) do
+                        local Option = Folder.Option:Clone()
+                        Option.Name = OptionName
+                        Option.Parent = Dropdown.Container.Holder.Container
+                        Option.Title.Text = OptionName
+                        Option.BackgroundColor3 = Config.Color
+                        Option.Size = UDim2.new(1, 0, 0, Option.Title.TextBounds.Y + 5)
+                        Dropdown.Container.Holder.Size = UDim2.new(1, -5, 0, Dropdown.Container.Holder.Container.ListLayout.AbsoluteContentSize.Y)
+                        table.insert(Library.ColorTable, Option)
+
+                        Option.MouseButton1Down:Connect(function()
+                            Option.BackgroundTransparency = 0
+                        end)
+
+                        Option.MouseButton1Up:Connect(function()
+                            Option.BackgroundTransparency = 1
+                        end)
+
+                        Option.MouseLeave:Connect(function()
+                            Option.BackgroundTransparency = 1
+                        end)
+
+                        Option.MouseButton1Click:Connect(function()
+                            Dropdown.Container.Value.Text = OptionName
+                            Callback(OptionName)
+
+                            DropdownToggle = false
+                            Dropdown.Size = UDim2.new(1, -10, 0, Dropdown.Title.TextBounds.Y + 25)
+                            Dropdown.Container.Holder.Visible = false
+                        end)
+                    end
                 end
 
                 function DropdownInit:RemoveOption(Name)
